52a9cc0614ae5c8042b4a24f4a30a781e6ec7f43
roshnamariyamrajan <roshnarajan3975@gmail.com>
2024-02-05 13:33:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function showHome() {
+    alert('Home page functionality');
+}
+
+function searchCars() {
+    alert('Search for available cars functionality');
+}
+
+function bookCar() {
+    alert('Book a car functionality');
+}
+
+function viewHistory() {
+    alert('View rental history functionality');
+}
